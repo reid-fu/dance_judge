64c0c39f24d7a5e2876b43de9f489d3a978501a5
BtLutz <b@btlutz.com>
2016-02-21 06:59:08 -0600

    Add UI

--- TotallyDance2/ViewController.swift
+++ TotallyDance2/ViewController.swift
@@ -12,15 +12,30 @@ import PhotosUI
 import MobileCoreServices
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, PHLivePhotoViewDelegate {
-
+    // MARK: Properties
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+            }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    @IBAction func buttonView(sender: UIButton) {
+        
+        // Use the UIImage in your UIImageView
+        textField.text = "good"
+        let jeremyGif = UIImage.gifWithName("IMG_0030_1")
+        let imageView = UIImageView(image: jeremyGif)
+        imageView.frame = CGRect(x: -150.0, y: 0.0, width: 700.0, height: 680.0)
+        
+        view.addSubview(imageView)
     }
 }
 
