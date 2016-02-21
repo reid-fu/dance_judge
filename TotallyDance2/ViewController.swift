9f88430b1e966b837ec3cee7fca0f7ae38cea00e
BtLutz <b@btlutz.com>
2016-02-20 22:43:24 -0600

    Initial Commit

---
+++ TotallyDance2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TotallyDance2
+//
+//  Created by Brian Lutz on 2/20/16.
+//  Copyright © 2016 Brian Lutz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
